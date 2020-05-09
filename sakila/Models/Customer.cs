23cb6e67e3e0ffc393ce302af1a262cc4ca7746c
Bradley Reynolds <breynolds3@my.ancoraed.com>
2020-05-09 13:33:20 -0500

    Add project files.

---
+++ sakila/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace sakila.Models
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Payment = new HashSet<Payment>();
+            Rental = new HashSet<Rental>();
+        }
+
+        public int CustomerId { get; set; }
+        public int StoreId { get; set; }
+        
+        [RegularExpression(@"^[A-Z]+[a-z]*")]
+        public string FirstName { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-z]*")] 
+        public string LastName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public int AddressId { get; set; }
+        public string Active { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdate { get; set; }
+
+        public Address Address { get; set; }
+        public Store Store { get; set; }
+        public ICollection<Payment> Payment { get; set; }
+        public ICollection<Rental> Rental { get; set; }
+    }
+}
